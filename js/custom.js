888e05d2f2bbc780e52bd21d9d5679ef2403f81c
William Beasley <chabot24@gmail.com>
2018-02-05 14:24:53 -0500

    yeah

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,1 @@
 var $j = jQuery.noConflict();
-var container = $j('#main_container');
-$j('head').load('/head.html');
-$j('body').prepend($j('<div>').load('/nav.html'));
-
-/* Change the div content to the page selected */
-$j('.link').on('click', function(e){
-	console.log($j(this));
-	if($j(this).data('name') == 'smk'){
-		$j(container).load('/smk/index.html');
-	}
-});
